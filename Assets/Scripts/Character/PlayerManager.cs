6f72c944e25121faa8cac408133d2b7ad1850503
pp <450457963@qq.com>
2022-10-04 09:09:15 +0800

    feature/animator

--- Assets/Scripts/Character/PlayerManager.cs
+++ Assets/Scripts/Character/PlayerManager.cs
@@ -5,16 +5,28 @@ using UnityEngine.AI;
 public class PlayerManager : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator anim;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         //MouseManager.Instance.OnMouseClick += OnMouseClick; // 因为这里的 MouseManager.Instance 可能为空
     }
 
     private void Start()
     {
         MouseManager.Instance.OnMouseClick += MoveToTarget;
+    }
+
+    private void Update()
+    {
+        SwitchAnimation();
+    }
+
+    private void SwitchAnimation()
+    {
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
     private void MoveToTarget(Vector3 vector) {

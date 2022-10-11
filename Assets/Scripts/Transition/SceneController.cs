3a086006adc45b2569e61645b131bbce3f3f3cb3
brownfeng <450457963@qq.com>
2022-10-11 22:24:36 +0800

    feature/创建第二个场景, 使用协程切换场景

--- Assets/Scripts/Transition/SceneController.cs
+++ Assets/Scripts/Transition/SceneController.cs
@@ -6,8 +6,17 @@ using UnityEngine.AI;
 
 public class SceneController : Singleton<SceneController>
 {
-    GameObject player;
-    NavMeshAgent playerAgent;
+    private GameObject player;
+    private NavMeshAgent playerAgent;
+
+    public GameObject playerPrefab;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this);
+    }
+
     public void TransitionToDestination(TransitionPoint transitionPoint)
     {
         switch (transitionPoint.transitionType)
@@ -17,6 +26,7 @@ public class SceneController : Singleton<SceneController>
                 StartCoroutine(Transition(SceneManager.GetActiveScene().name, transitionPoint.destinationTag));
                 break;
             case TransitionPoint.TransitionType.DifferentScene:
+                StartCoroutine(Transition(transitionPoint.sceneName, transitionPoint.destinationTag));
                 break;
         }
  
@@ -24,13 +34,26 @@ public class SceneController : Singleton<SceneController>
 
     IEnumerator Transition(string sceneName, TransitionDestination.DestinationTag destinationTag)
     {
-        player = GameManager.Instance.playerStats.gameObject;
-        playerAgent = player.GetComponent<NavMeshAgent>();
-        playerAgent.enabled = false;
-        var transform = GetDestination(destinationTag).transform;
-        player.transform.SetPositionAndRotation(transform.position, transform.rotation);
-        playerAgent.enabled = true;
-        yield return null;
+        // TODO: 传送的时候, 保存数据
+        // TODO: 
+        if (SceneManager.GetActiveScene().name != sceneName) {
+            // 异步加载场景
+            yield return SceneManager.LoadSceneAsync(sceneName);
+            // 异步生成角色
+            var transform = GetDestination(destinationTag).transform;
+            yield return Instantiate(playerPrefab, transform.position, transform.rotation);
+            yield break;
+        } else {
+            // 相同场景
+            player = GameManager.Instance.playerStats.gameObject;
+            playerAgent = player.GetComponent<NavMeshAgent>();
+            playerAgent.enabled = false;
+            var transform = GetDestination(destinationTag).transform;
+            player.transform.SetPositionAndRotation(transform.position, transform.rotation);
+            playerAgent.enabled = true;
+            yield return null;
+        }
+
     }
 
     private TransitionDestination GetDestination(TransitionDestination.DestinationTag destinationTag)
